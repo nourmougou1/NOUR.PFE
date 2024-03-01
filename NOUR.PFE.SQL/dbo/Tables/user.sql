94491514f0b480b56a3b1675f93d6a2901a1e1f5
nourmougou1 <nour.mougou@issatm.ucar.tn>
2024-03-01 10:40:07 +0100

    Commit Nour 01-03-2024 10:39

---
+++ NOUR.PFE.SQL/dbo/Tables/user.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[user]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [first_name] NCHAR(10) NULL, 
+    [email] NCHAR(10) NULL, 
+    [password] NCHAR(10) NULL, 
+    [login] NCHAR(10) NULL, 
+    [role_id] NCHAR(10) NULL, 
+    [user_phone] NCHAR(10) NULL, 
+    [user_birthday] NCHAR(10) NULL, 
+    [last_name] NCHAR(10) NULL
+)
---
+++ NOUR.PFE.SQL/dbo/Tables/user_role.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[user_role]
+(
+	[role_id] INT NOT NULL PRIMARY KEY, 
+    [role_name] NCHAR(10) NULL, 
+    [email] NCHAR(10) NULL
+)
