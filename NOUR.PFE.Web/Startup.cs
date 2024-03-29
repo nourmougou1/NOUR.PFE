1395c359e154ef45522715f9704267cd4f03fbe9
nourmougou1 <nour.mougou@issatm.ucar.tn>
2024-03-29 21:50:54 +0100

    test

--- NOUR.PFE.Web/Startup.cs
+++ NOUR.PFE.Web/Startup.cs
@@ -46,6 +46,8 @@ namespace NOUR.PFE.Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSession();
+
             app.UseRouting();
 
             app.UseAuthorization();
